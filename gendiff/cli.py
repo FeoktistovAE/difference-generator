b55354bf06d7edb292ec009ce9cfd3ef1b81d4e6
FeoktistivAE <feoae@yandex.ru>
2022-10-02 12:52:38 +0300

    review fix 6

---
+++ gendiff/cli.py
@@ -0,0 +1,13 @@
+import argparse
+from gendiff import generate_diff
+
+def run_gendiff():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of input: plain or stylish',
+                        type=str, default='stylish')
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file, args.format))
